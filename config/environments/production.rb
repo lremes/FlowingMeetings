7fa95e793473b4a1dbfe95b26d5e829ec880d804
Lars Remes <lars.remes@gmail.com>
2021-02-20 16:06:41 +0200

    Add sample nginx config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,10 +47,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://knkk.qs.fi/cable'
-  config.action_cable.allowed_request_origins = [ 'http://knkk.qs.fi' ]
+  config.action_cable.allowed_request_origins = [ 'https://knkk.qs.fi' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
